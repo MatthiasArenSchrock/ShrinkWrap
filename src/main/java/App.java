25de4520b0c8ce225b64df8795df143f4ee15944
Matthias Schrock <mxs21j@acu.edu>
2024-04-25 12:19:34 -0500

    Merging Main and Dev

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,10 +4,8 @@
  */
 public class App 
 {
-    // Testing pre-push git hook (no T.O.D.O.s in comments)
     public static void main( String[] args )
     {
-        //TODO: should be removed before attempting to push to main branch but will push to dev
         System.out.println( "Hello World!" );
     }
 }
