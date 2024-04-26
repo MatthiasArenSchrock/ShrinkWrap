64d9a59ca882dcb6d43b73769853b949c54b6819
Matthias Schrock <mxs21j@acu.edu>
2024-04-26 11:53:04 -0500

    Bout Test

--- src/test/java/IO/BinTest.java
+++ src/test/java/IO/BinTest.java
@@ -10,6 +10,7 @@ package IO;
  * Execute     : mvn test
  */
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,12 +18,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class BinTest {
     private Bin bin;
     private Path dir, testFile;
+    private static final String BLEE = "Blee\nBlah\nBlue";
 
     @Before
     public void setUp() throws IOException {
@@ -30,13 +33,97 @@ public class BinTest {
         Files.createDirectories(dir);
 
         testFile = dir.resolve("Blee.txt");
-        Files.createFile(testFile);
+        Files.write(testFile, BLEE.getBytes(), StandardOpenOption.CREATE);
 
         bin = new Bin(testFile.toString());
+        assertNotNull(bin);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        bin.close();
     }
 
     @Test
-    public void test() throws IOException {
-        assertTrue(Files.exists(testFile));
+    public void testIsEmpty() throws IOException {
+        assertFalse(bin.isEmpty());
+        bin.readAllBytes();
+        assertTrue(bin.isEmpty());
+    }
+
+    @Test
+    public void testReadString() throws IOException {
+        assertEquals(BLEE, bin.readString());
+    }
+
+    @Test
+    public void testReadAllBytes() throws IOException {
+        assertArrayEquals(BLEE.getBytes(), bin.readAllBytes());
+    }
+
+    @Test
+    public void testReadChar() throws IOException {
+        int i = 0;
+        while(!bin.isEmpty()) {
+            assertEquals(BLEE.charAt(i++), bin.readChar());
+        }
+    }
+
+    @Test(expected = IOException.class)
+    public void testReadCharWithEmptyStream() throws IOException {
+        while(!bin.isEmpty()) {
+            bin.readChar();
+        }
+
+        bin.readChar();
+    }
+
+    @Test
+    public void testReadInt() throws IOException {
+        int expected = 0;
+        for (int i = 0; i < 4; i++) {
+            expected <<= 8;
+            expected |= BLEE.charAt(i);
+        }
+
+        assertEquals(expected, bin.readInt());
+    }
+
+    @Test
+    public void testReadIntWithR() throws IOException {
+        int i = 0;
+        while(!bin.isEmpty()) {
+            assertEquals((int) BLEE.charAt(i++), bin.readInt(8));
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testReadIntWithRAboveMax() throws IOException {
+        bin.readInt(33);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testReadIntWithRBelowMin() throws IOException {
+        bin.readInt(0);
+    }
+
+    @Test
+    public void testReadLong() throws IOException {
+        long expected = 0;
+        for (int i = 0; i < 8; i++) {
+            expected <<= 8;
+            expected |= BLEE.charAt(i);
+        }
+
+        assertEquals(expected, bin.readLong());
+    }
+
+    @Test
+    public void testReadBit() throws IOException {
+        for (char c : BLEE.toCharArray()) {
+            for (int i = 7; i >= 0; i--) {
+                assertEquals(((c >> i) & 1) == 1, bin.readBit());
+            }
+        }
     }
 }
--- src/test/java/IO/BoutTest.java
+++ src/test/java/IO/BoutTest.java
@@ -10,5 +10,170 @@ package IO;
  * Execute     : mvn test
  */
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class BoutTest {
+    private Bout bout;
+    private Path dir, testFile;
+    private static final String BLEE = "Blee\nBlah\nBlue";
+
+    @Before
+    public void setUp() throws IOException {
+        dir = Paths.get("src", "test", "resource");
+        Files.createDirectories(dir);
+
+        testFile = dir.resolve("Blee.txt");
+        Files.write(testFile, new byte[0], StandardOpenOption.CREATE);
+
+        bout = new Bout(testFile.toString());
+        assertNotNull(bout);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        if (bout != null) {
+            bout.close();
+        }
+    }
+
+    @Test
+    public void testWriteBit() throws IOException {
+        for (char c : BLEE.toCharArray()) {
+            for (int i = 7; i >= 0; i--) {
+                bout.writeBit(((c >> i) & 1) == 1);
+            }
+        }
+        bout.close();
+
+        testContent(BLEE);
+    }
+
+    @Test
+    public void testWriteByte() throws IOException {
+        for (char c : BLEE.toCharArray()) {
+            bout.writeByte(c);
+        }
+        bout.close();
+
+        testContent(BLEE);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testWriteByteGreaterThanMax() throws IOException {
+        bout.writeByte(256);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testWriteByteLessThanMin() throws IOException {
+        bout.writeByte(-1);
+    }
+
+    @Test
+    public void testWriteChar() throws IOException {
+        for (char c : BLEE.toCharArray()) {
+            bout.write(c);
+        }
+        bout.close();
+
+        testContent(BLEE);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testWriteCharGreaterThanMax() throws IOException {
+        bout.write((char) 256);
+    }
+
+    @Test
+    public void testWriteString() throws IOException {
+        bout.write(BLEE);
+        bout.close();
+
+        testContent(BLEE);
+    }
+
+    @Test
+    public void testWriteInt() throws IOException {
+        for (char c : BLEE.toCharArray()) {
+            bout.write((int) c);
+        }
+        bout.close();
+
+        char[] content = new String(Files.readAllBytes(testFile)).toCharArray();
+        int n = 0;
+        for (int i = 3; i < content.length; i+=4) {
+            assertEquals(BLEE.charAt(n++), content[i]);
+        }
+    }
+
+    @Test
+    public void testWriteRbitInt() throws IOException {
+        for (char c : BLEE.toCharArray()) {
+            bout.write((int) c, 8);
+        }
+        bout.close();
+
+        testContent(BLEE);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testWriteRbitIntWithLenGreaterThanMax() throws IOException {
+        bout.write(256, 33);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testWriteRbitIntWithLenLessThanMin() throws IOException {
+        bout.write(256, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testWriteRbitIntGreaterThanMax() throws IOException {
+        bout.write(256, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testWriteRbitIntLessThanMin() throws IOException {
+        bout.write(-1, 1);
+    }
+
+    @Test
+    public void testWriteLong() throws IOException {
+        for (char c : BLEE.toCharArray()) {
+            bout.write((long) c);
+        }
+        bout.close();
+
+        char[] content = new String(Files.readAllBytes(testFile)).toCharArray();
+        int n = 0;
+        for (int i = 7; i < content.length; i+=8) {
+            assertEquals(BLEE.charAt(n++), content[i]);
+        }
+    }
+
+    @Test
+    public void testFlush() throws IOException {
+        for (char c : BLEE.toCharArray()) {
+            for (int i = 7; i >= 0; i--) {
+                bout.writeBit(((c >> i) & 1) == 1);
+            }
+        }
+
+        bout.flush();
+        testContent(BLEE);
+    }
+
+    private void testContent(String expected) throws IOException {
+        String content = new String(Files.readAllBytes(testFile));
+        assertEquals(expected, content);
+    }
 }
