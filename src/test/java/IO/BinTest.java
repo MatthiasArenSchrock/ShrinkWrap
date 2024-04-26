8bcecc9752d5680361683f2d290d4a7a410db7d3
Matthias Schrock <mxs21j@acu.edu>
2024-04-26 10:17:37 -0500

    Setup Bin Test

--- src/test/java/IO/BinTest.java
+++ src/test/java/IO/BinTest.java
@@ -10,5 +10,33 @@ package IO;
  * Execute     : mvn test
  */
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertTrue;
+
 public class BinTest {
+    private Bin bin;
+    private Path dir, testFile;
+
+    @Before
+    public void setUp() throws IOException {
+        dir = Paths.get("src", "test", "resource");
+        Files.createDirectories(dir);
+
+        testFile = dir.resolve("Blee.txt");
+        Files.createFile(testFile);
+
+        bin = new Bin(testFile.toString());
+    }
+
+    @Test
+    public void test() throws IOException {
+        assertTrue(Files.exists(testFile));
+    }
 }
